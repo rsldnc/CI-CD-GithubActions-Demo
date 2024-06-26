f67ce8e2fbdc30bacbdcdf983ca16bf9c3a045dc
Resul Dinc <dincresull@gmail.com>
2024-06-26 17:50:16 +0300

    manualtest created for unittest fail check

---
+++ src/test/java/com/rsldnc/actions/ManualTest.java
@@ -0,0 +1,13 @@
+package com.rsldnc.actions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ManualTest {
+    @Test
+    public void test1() {
+        int a = 3;
+        assertEquals(1, a);
+    }
+}
